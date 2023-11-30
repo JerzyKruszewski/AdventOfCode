9e000b73aaa00e16a6673a8e4030072993ab3348
JerzyKruszewski <JerzyJ.Kruszewski@gmail.com>
2023-11-30 21:18:14 +0100

    Initial commit with AoC 2023 project structure

---
+++ AoC2023.Benchmarks/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Threading.Tasks;
+global using BenchmarkDotNet;
+global using BenchmarkDotNet.Diagnosers;
+global using BenchmarkDotNet.Running;
---
+++ AoC2023.Benchmarks/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace AoC2023.Benchmarks;
+
+internal sealed class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
